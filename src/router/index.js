45831acfd8ed5ae27494c7462f993cb723846ad5
juanamolinalucia2001 <juana2001molina@gmail.com>
2023-02-14 14:16:42 -0300

    proyecto final

--- src/router/index.js
+++ src/router/index.js
@@ -28,7 +28,7 @@ const routes = [
     path: '/',
     name: 'chat',
     component: () => import(/* webpackChunkName: "chat" */ '../views/Chat.vue'),
-    //meta: { requireAuth: true}
+    meta: { requireAuth: true}
   },
 ]
 
